63065f9265393e651709ccefdcff7cfa46b8f6a1
frumorn <andreabrescia002@gmail.com>
2024-09-27 18:02:03 +0200

    Gioco n sedie primo esercizio

--- Esercizi/Lock e Condition/Gioco Delle N sedie/GiocoNSedie.py
+++ Esercizi/Lock e Condition/Gioco Delle N sedie/GiocoNSedie.py
@@ -21,8 +21,8 @@ class Posto:
         self.occupato = False # Posto libero o occupato
         self.lock = Lock() # Lock per la mutua esclusione
 
-    def testANDsit(self):
-        with self.lock:
+    def testANDsit(self):  # Qui cosa faccio: se il posto è libero lo occupo e ritorno True, altrimenti ritorno False
+        with self.lock:  # with self.lock: è un modo per fare il lock.acquire() e lock.release() in modo automatico e sicuro
             if self.occupato:
                 return False
             self.occupato = True
@@ -30,9 +30,14 @@ class Posto:
 
 
 class Partecipante(Thread):
+
+    # Qui passo i posti in modo che il partecipante possa sedersi
     def __init__(self,posti):
         super().__init__()
         self.posti = posti
+
+    # Qui il partecipante cerca di sedersi su una sedia libera
+    # Se tutte le sedie sono occupate, il partecipante termina il suo thread
 
     def run(self):
         sleep(randrange(1,5)) # Attesa casuale
@@ -43,6 +48,12 @@ class Partecipante(Thread):
 
         print(f"Tutte le sedie sono occupate, sono {self.name}")
 
+
+
+# Qui creo un thread che si occupa di visualizzare lo stato delle sedie
+# Ogni secondo, il thread visualizza lo stato delle sedie
+# Se una sedia è occupata, viene visualizzata con un "*"
+# Se una sedia è libera, viene visualizzata con un "o"
 
 class Display(Thread):
     def __init__(self,posti):
@@ -62,6 +73,12 @@ class Display(Thread):
             sleep(1)
 
 
+
+
+# Qui creo le sedie e i partecipanti e avvio il programma
+# Creo N posti e N+1 partecipanti
+# Avvio il thread display che visualizza lo stato delle sedie
+# Avvio i thread partecipanti
 
 def main():
     N = 10 # Numero di posti
