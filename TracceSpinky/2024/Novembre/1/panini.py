9ae45d8fc1aade967c188e9e8919969ff536034e
alvin <ddrndr02d27d086h@studenti.unical.it>
2025-05-08 12:01:47 +0200

    Co-authored-by: Andrea Brescia <Frumorn12@users.noreply.github.com>

--- TracceSpinky/2024/Novembre/1/panini.py
+++ TracceSpinky/2024/Novembre/1/panini.py
@@ -3,8 +3,22 @@ import queue
 import time
 import random
 
+# Importo Rlock
+from threading import RLock 
+
+lock_print = RLock() 
+lock_panini = RLock() 
+condition = threading.Condition(lock_panini) 
+lista_storico = [] #
+num_max_ingredienti = 20 # Punto 4: numero massimo di ingredienti per ogni tipo
+storico_ingredienti = [0,0,0,0,0,0,0] # Punto 4: Ad ogni indice corrisponde un ingrediente quando arriva a 20 non si può usare più quell'ingrediente
+
+
+
+
+
 # Liste di code per ingredienti (codici da 0 a 4)
-code_ingredienti = [queue.Queue() for _ in range(5)]
+code_ingredienti = [queue.Queue() for _ in range(7)]
 
 #
 # code_ingredienti[0] => Coda Lattuga
@@ -18,20 +32,36 @@ simboli = {
             1: "====",  # Bacon
             2: "####",  # Hamburger
             3: "----",  # Formaggio
-            4: "oooo"   # Pomodoro
+            4: "oooo",   # Pomodoro
+            #Punto 1
+            5: "/==\\",   #Panino Sopra
+            6: "\==/",   #Panino Sotto
 }
 nomi = {
             0: "Lattuga",    # Lattuga
             1: "Bacon",      # Bacon
             2: "Hamburger",  # Hamburger
-            3: "Formaggio",  # Formaggio
-            4: "Pomodoro"    # Pomodoro
+            3: "Formaggio",  # Foramaggio
+            4: "Pomodoro",    # Pomodoro
+            5: "Panino Sopra", # Panino Sopra
+            6: "Panino Sotto"  # Panino Sotto
 }
 
 # 
 # Coda per gli ordini
 #
 coda_ordini = queue.Queue()
+
+def put_coda_ordini(ordine, ifWait=False):
+    with lock_panini:
+        coda_ordini.put(ordine)
+        if ifWait:
+            numero_ordine = ordine[0]
+            while numero_ordine not in lista_storico:
+                print (f"Ordine {numero_ordine} in attesa, sono entrato nel wait.")
+                condition.wait()  
+            print (f"Ordine {ordine} completato, sono uscito dal wait.")
+        
 
 #
 # Thread produttore di ingredienti
@@ -43,10 +73,18 @@ class ProduttoreIngredienti(threading.Thread):
 
     def run(self):
         while True:
+            #Punto 4:
+            if storico_ingredienti[self.codice_ingrediente] >= num_max_ingredienti:
+                print(f"Ingredienti {nomi[self.codice_ingrediente]} esauriti, non posso più produrli.")
+                return
             # Produce l'ingrediente e lo mette nella coda specifica
-            ingrediente = f"Ingrediente {self.codice_ingrediente}"
-            code_ingredienti[self.codice_ingrediente].put(ingrediente)
+            code_ingredienti[self.codice_ingrediente].put(self.codice_ingrediente)
             print(f"Prodotto {nomi[self.codice_ingrediente]}")
+            
+            # Punto 4: Incremento il contatore degli ingredienti
+            storico_ingredienti[self.codice_ingrediente] += 1        
+
+            
             # Simula il tempo di produzione
             time.sleep(random.uniform(0.5, 1.5))
 
@@ -59,24 +97,52 @@ class Assemblatore(threading.Thread):
 
     def run(self):
         while True:
-            # Prende un ordine dalla coda degli ordini
+            # Prende un ordine dalla coda degli ordini e lo toglie dalla coda
             ordine_id, ordine = coda_ordini.get()
             if ordine is None:
                 break  # Fine degli ordini
 
             assemblato = []
+            
+            #Punto 1: aggiungo pane sopra
+            assemblato.append(code_ingredienti[5].get())
+
             print(f"Assemblaggio ordine {ordine_id}: {ordine}")
             # Preleva gli ingredienti richiesti dall'ordine
             for codice_ingrediente in ordine:
                 ingrediente_preso = code_ingredienti[codice_ingrediente].get()
                 assemblato.append(ingrediente_preso)
                 print(f"Prelevato {ingrediente_preso} per l'ordine {ordine_id}")
-                self.stampa_assemblaggio(codice_ingrediente)
+                
             
-            print(f"Ordine {ordine_id} completato: {assemblato}")
+            #Punto 1: aggiungo pane sotto
+            assemblato.append(code_ingredienti[6].get())
+            with lock_print:
+                # Stampa l'assemblaggio finale
+                print(f"Assemblaggio finale per ordine {ordine_id}:")
+                for i in range (len(assemblato)):
+                    self.stampa_assemblaggio(assemblato[i])
+                print(f"Ordine {ordine_id} completato: {assemblato}")
+                print("===================================")
+            
+            lista_storico.append(ordine_id)
+            condition.notify_all()  # Notifica che l'ordine è stato completato 
+            
 
     def stampa_assemblaggio(self, codice_ingrediente):
-       print(simboli[codice_ingrediente])
+        print(simboli[codice_ingrediente])
+
+class InviatoreOrdiniAvanzato(threading.Thread):
+    def __init__(self, ordine_id, ordini):
+        super().__init__()
+        self.ordine_id = ordine_id
+        self.ordini = ordini
+    def run(self):
+        for ordine in self.ordini:
+            print(f"Thread {self.ordine_id} ha inviato ordine: {ordine}")
+            put_coda_ordini((self.ordine_id, ordine), True) 
+            time.sleep(2)
+    
 
 # Thread che genera gli ordini
 class InviatoreOrdini(threading.Thread):
@@ -93,7 +159,7 @@ class InviatoreOrdini(threading.Thread):
 
 # Avvio dei thread produttori per ciascun ingrediente
 thread_ingredienti = []
-for codice in range(5):
+for codice in range(7):
     thread = ProduttoreIngredienti(codice)
     thread.start()
     thread_ingredienti.append(thread)
