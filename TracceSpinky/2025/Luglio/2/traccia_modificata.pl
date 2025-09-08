349b930461eaa84d94bdf05d0aa33e529386f829
Frumorn12 <andreabrescia002@gmail.com>
2025-09-08 12:20:48 +0200

    perl luglio 25

--- TracceSpinky/2025/Luglio/2/traccia_modificata.pl
+++ TracceSpinky/2025/Luglio/2/traccia_modificata.pl
@@ -1,25 +1,64 @@
-$comando=$ARGV[1];
-$stringa=$ARGV[2];
-$linee=$ARGV[3];
 
-@ris = qx($comando | grep -C  $stringa)
+use warnings; 
+
+# controllo che il numero di argomenti sia corretto
+
+
+if (@ARGV!= 3){
+    die "Numero di argomenti errato"; 
+}
+
+# si parte da 0
+# $comando=$ARGV[1];
+# $stringa=$ARGV[2];
+# $linee=$ARGV[3];
+
+$comando=$ARGV[0];
+$stringa=$ARGV[1];
+$linee=$ARGV[2];
+# comando sbagliato. Dobbiamo prendere la documentazione di $comando quindi 
+# dobbiamo aggiungiere il flag --help. Inoltre grep ha bisogno con il flag -C di un numero 
+# dopo, in questo caso linee
+
+# SBAGLIATO ANGILICA.
+# mancava il flag --help, e inoltre il -C vuole un numero dopo .
+# @ris = qx($comando | grep -C  $stringa)
+# CORRETTO
+@ris = qx(man $comando| grep -C $linee $stringa); 
+
+
 $cont=0;
 $max_cont=0;
+
+@temp=(); 
 for(@ris){
-    if(/\s*--\s*/){
-        if($cont==$max_cont){
+    # regex: 
+    # // indica che è una regex 
+    # \s* -> 0 o più spazi bianchi
+    # -- -> due trattini
+    # \s* -> 0 o più spazi bianchi 
+    if(/\s*--\s*$/){
+        if($cont>$max_cont){
             @da_stampare=@temp;
-            $cont=$max_cont;
+            # $cont=$max_cont;
+            $max_cont=$cont; 
         }
         @temp=();
         $cont=0;
     }
     else{
         $cont+=1;
-        push $_,@temp;
+        # push $_,@temp;
+        
+        push @temp,$_; 
     }
 }
-if($cont==$max_cont){
-    $cont=0;
+# negli if ovviamente era sbagliato l'== era maggiore > 
+if($cont>$max_cont){
+    $max_cont=$cont; 
+    @da_stampare=@temp; 
 }
-print @da_stampare;
+
+for (@da_stampare){
+    print $_; 
+}
--- filetest.pl
+++ filetest.pl
@@ -1,9 +1,41 @@
 
+use warnings; 
 
-$stringa_cercata="report"; 
+# controllo che il numero di argomenti sia corretto
 
-@array = qx(cat "filetest.txt" | grep -P '$stringa_cercata\\S*\$' | sort -k 4 -b ); 
 
-for(@array){
-    print "$_\n";
-} 
+if (@ARGV!= 3){
+    die "Numero di argomenti errato"; 
+}
+
+# si parte da 0
+# $comando=$ARGV[1];
+# $stringa=$ARGV[2];
+# $linee=$ARGV[3];
+
+my $comando=$ARGV[0];
+my $stringa=$ARGV[1];
+my $linee=$ARGV[2];
+# comando sbagliato. Dobbiamo prendere la documentazione di $comando quindi 
+# dobbiamo aggiungiere il flag --help. Inoltre grep ha bisogno con il flag -C di un numero 
+# dopo, in questo caso linee
+# @ris = qx($comando | grep -C  $stringa)
+
+@ris = qx(man $comando | grep -C $linee $stringa); 
+print "DEBUG: Risultato del comando:\n";
+print "Comando: $comando --help | grep -C $linee $stringa\n";
+
+$cont=0;
+for (@ris){
+    print "Linea $cont: ";
+    $cont++; 
+    print $_;
+}
+
+# my $count =0; 
+# for (@ris){
+#     chomp;
+#     print "BLOCCO $count:  ";
+#     print "$_\n";
+#     $count++; 
+# }
